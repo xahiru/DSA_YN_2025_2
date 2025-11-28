6b527084227617d9bdd0cc93d56e4a069c18f424
Ahmed Zahir <xahiru@gmail.com>
2025-11-28 21:36:21 +0800

    previous one

--- Chapters/1/strings/main.c
+++ Chapters/1/strings/main.c
@@ -1,3 +1,37 @@
+// #include <stdio.h>
+// #include "adt_string.h"
+
+// int main() {
+//     String S = CreateFromChars("ComputerScience");
+
+//     printf("Original: %s\n", S.data);
+
+//     // Substring
+//     String sub = Substring(S, 4, 6);
+//     printf("Substring(4,6): %s\n", sub.data);
+
+//     // CharAt
+//     printf("CharAt(3): %c\n", CharAt(S, 3));
+
+//     // IsEmpty
+//     printf("IsEmpty: %d\n", IsEmpty(S));
+
+//     // Compare
+//     String A = CreateFromChars("Apple");
+//     String B = CreateFromChars("Banana");
+//     printf("Compare(Apple, Banana) = %d\n", Compare(A, B));
+
+//     DestroyString(&S);
+//     DestroyString(&sub);
+//     DestroyString(&A);
+//     DestroyString(&B);
+
+//     return 0;
+// }
+
+
+
+
 #include <stdio.h>
 #include "adt2_sstring.h"
 
