60565c58b2c1fc62cf95150f525e5c26b5d4c867
Ahmed Zahir <xahiru@gmail.com>
2025-11-28 21:30:32 +0800

    more func

--- Chapters/1/strings/adt_sting.c
+++ Chapters/1/strings/adt_sting.c
@@ -9,11 +9,11 @@ String CreateEmptyString() {
     String S;
     S.length = 0;
     S.data = (char *)malloc(1);
-    S.data[0] = '\0';       // empty string
+    S.data[0] = '\0';
     return S;
 }
 
-// Return length of string
+// Get string length
 int StringLength(String S) {
     return S.length;
 }
@@ -22,17 +22,52 @@ int StringLength(String S) {
 String Concatenate(String S1, String S2) {
     String R;
     R.length = S1.length + S2.length;
-
-    R.data = (char *)malloc(R.length + 1); // +1 for '\0'
+    R.data = (char *)malloc(R.length + 1);
 
     memcpy(R.data, S1.data, S1.length);
     memcpy(R.data + S1.length, S2.data, S2.length);
-
     R.data[R.length] = '\0';
+
     return R;
 }
 
-// Create a String from a C-string
+// Substring: start is 1-based index
+String Substring(String S, int start, int length) {
+    if (start < 1 || start > S.length || length < 0)
+        return CreateEmptyString();
+
+    int maxLen = S.length - (start - 1);
+    if (length > maxLen)
+        length = maxLen;
+
+    String R;
+    R.length = length;
+    R.data = (char *)malloc(length + 1);
+
+    memcpy(R.data, S.data + (start - 1), length);
+    R.data[length] = '\0';
+
+    return R;
+}
+
+// CharAt: 1-based index
+char CharAt(String S, int position) {
+    if (position < 1 || position > S.length)
+        return '\0';
+    return S.data[position - 1];
+}
+
+// Check if string is empty
+int IsEmpty(String S) {
+    return S.length == 0;
+}
+
+// Compare lexicographically
+int Compare(String S1, String S2) {
+    return strcmp(S1.data, S2.data);
+}
+
+// Create from C-string
 String CreateFromChars(const char *chars) {
     String S;
     S.length = strlen(chars);
@@ -41,9 +76,9 @@ String CreateFromChars(const char *chars) {
     return S;
 }
 
-// Free memory allocated for a String
+// Free allocated memory
 void DestroyString(String *S) {
-    if (S->data != NULL) {
+    if (S->data) {
         free(S->data);
         S->data = NULL;
     }
