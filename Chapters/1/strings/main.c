911e7a85331805c3dcfbe1afb7683f75c9a8b5b1
Ahmed Zahir <xahiru@gmail.com>
2025-11-28 21:35:36 +0800

    implementation2

--- Chapters/1/strings/main.c
+++ Chapters/1/strings/main.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
-#include "adt_string.h"
+#include "adt2_sstring.h"
+
+void PrintSString(SString S) {
+    for (int i = 1; i <= S[0]; i++)
+        putchar(S[i]);
+}
 
 int main() {
-    String S = CreateFromChars("ComputerScience");
+    SString A, B, C, Sub;
 
-    printf("Original: %s\n", S.data);
+    StrAssign(A, "Hello");
+    StrAssign(B, "World!");
+
+    printf("A = ");
+    PrintSString(A);
+    printf("\n");
+
+    printf("B = ");
+    PrintSString(B);
+    printf("\n");
+
+    // Concat
+    Concat(C, A, B);
+    printf("Concat(A,B) = ");
+    PrintSString(C);
+    printf("\n");
 
     // Substring
-    String sub = Substring(S, 4, 6);
-    printf("Substring(4,6): %s\n", sub.data);
-
-    // CharAt
-    printf("CharAt(3): %c\n", CharAt(S, 3));
-
-    // IsEmpty
-    printf("IsEmpty: %d\n", IsEmpty(S));
+    SubString(Sub, C, 6, 3);
+    printf("Substring(C,6,3) = ");
+    PrintSString(Sub);
+    printf("\n");
 
     // Compare
-    String A = CreateFromChars("Apple");
-    String B = CreateFromChars("Banana");
-    printf("Compare(Apple, Banana) = %d\n", Compare(A, B));
-
-    DestroyString(&S);
-    DestroyString(&sub);
-    DestroyString(&A);
-    DestroyString(&B);
+    printf("Compare(A,B) = %d\n", StrCompare(A, B));
 
     return 0;
 }
